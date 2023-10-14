eddacb75c0233f59b9ddbf37d869f3cfbd10df27
ZinoChan <devzinochan@gmail.com>
2023-10-14 12:20:14 +0100

    feat: implement mission preview page

---
+++ src/app/profile/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import Post from '@/shared/Post';
+
+const UserMissionDetails = () => {
+  return (
+    <section className='pt-10 '>
+      <div className='mx-auto max-w-screen-md'>
+        <Post editBtns />
+      </div>
+    </section>
+  );
+};
+
+export default UserMissionDetails;

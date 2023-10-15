93247fd62011ca59321abcb638c30599e108dcc4
ZinoChan <devzinochan@gmail.com>
2023-10-15 09:20:29 +0100

    feat: implement edit mission pages

--- src/components/profile/EditBtns.tsx
+++ src/components/profile/EditBtns.tsx
@@ -1,11 +1,16 @@
+import { ROUTES } from '@/utils/routes';
+import Link from 'next/link';
 import { BiEdit, BiTrash } from 'react-icons/bi';
 
-const EditBtns = () => {
+const EditBtns = ({ mission_slug }: { mission_slug: string }) => {
   return (
     <div className='flex items-baseline space-x-3'>
-      <button className='text-secondary-3 hover:text-purple-500'>
+      <Link
+        href={`${ROUTES.USER_PROFILE}/${mission_slug}/edit-mission/step_1`}
+        className='text-secondary-3 hover:text-purple-500'
+      >
         <BiEdit className='text-xl' />
-      </button>
+      </Link>
       <button className='text-red-500 hover:text-red-400'>
         <BiTrash className='text-xl' />
       </button>

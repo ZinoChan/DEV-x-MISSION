68ec132ba3f3112aeee3db774c16f5c4fd6c9c4a
ZinoChan <devzinochan@gmail.com>
2023-10-17 15:03:42 +0100

    refactor: make mission creation server side rendred
    move mission creation forms to client side
    use check user session in server
    change step 2 and 3 to dynamic pages

---
+++ src/app/create-mission/step_2/[missionId]/page.tsx
@@ -0,0 +1,34 @@
+import { authOptions } from '@/utils/AuthOptions';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import MissionFormStep2 from '../MissionFormStep2';
+import { xprisma } from '@/lib/prismaExtentions';
+import { ROUTES } from '@/utils/routes';
+
+interface Props {
+  params: {
+    missionId: string;
+  };
+}
+
+export default async function CreateMissionStep_2({ params }: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect('/api/auth/signin');
+  const currentUserEmail = session?.user?.email;
+
+  if (currentUserEmail == null) return null;
+
+  const user = await xprisma.user.findByEmail(currentUserEmail);
+
+  if (!user) return null;
+  const mission = await xprisma.mission.findMissionById(params.missionId);
+
+  if (mission == null) redirect(ROUTES.CREATE_MISSION_STEP_1);
+
+  return (
+    <>
+      <MissionFormStep2 mission={mission} />
+    </>
+  );
+}

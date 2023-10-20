f980b093a92ec1de97b9660a41445a674eb2a284
ZinoChan <devzinochan@gmail.com>
2023-10-20 15:58:05 +0100

    refactor: improve error handling
    add error page
    improve prisma error maper
    extend Error for more err information

--- src/utils/ErrHandling/HandleErr.tsx
+++ src/utils/ErrHandling/HandleErr.tsx
@@ -1,14 +1,32 @@
-import prismaErrorMsg from './PrismaErrorMaper';
+import { HTTP_STATUS } from '@/constants/httpStatus';
+import { HttpException } from './HttpException';
+import { getPrismaErrorMessage } from './PrismaErrorMaper';
 import { Prisma } from '@prisma/client';
 
-export function handleErrMsg(error: unknown): string {
-  if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    return prismaErrorMsg(error.message);
-  } else if (error instanceof Error) {
-    return error.message.length > 0
-      ? error.message
-      : 'failed to create mission';
-  } else {
-    return 'An unknown error occurred';
+export function handleErrMsg(error: unknown) {
+  let status;
+  let message;
+  if (isPrismaError(error)) {
+    const prismaErr = getPrismaErrorMessage(
+      error as Prisma.PrismaClientKnownRequestError
+    );
+    message = prismaErr.message;
+    status = prismaErr.httpStatus;
+  } else if (error instanceof HttpException) {
+    message = error.message;
+    status = error.status;
   }
+  if (status == null) status = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  if (message == null) message = 'An unknown error occurred';
+  return { status, message };
+}
+
+export function isPrismaError(error: unknown) {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError ||
+    error instanceof Prisma.PrismaClientUnknownRequestError ||
+    error instanceof Prisma.PrismaClientValidationError ||
+    error instanceof Prisma.PrismaClientInitializationError ||
+    error instanceof Prisma.PrismaClientRustPanicError
+  );
 }

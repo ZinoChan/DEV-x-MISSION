d089e2c9fdae987d5d077897b2195fe10fbbf1c4
ZinoChan <devzinochan@gmail.com>
2023-10-24 16:16:45 +0100

    refactor: enhance mission card ui

---
+++ src/constants/missionData.ts
@@ -0,0 +1,11 @@
+export const MISSIONS_STATUS = {
+  IDEATION: 'ideation',
+  PROTOTYPE: 'prototype',
+  DEVELOPMENT: 'development',
+  TESTING: 'testing',
+};
+export const SKILL_LEVELS = {
+  BEGINNER: 'beginner',
+  INTERMEDIATE: 'intermediate',
+  ADVANCED: 'advanced',
+};

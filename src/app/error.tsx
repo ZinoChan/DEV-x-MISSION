6c01c3968102cdb1e3a29f03e0db2d6ca546f395
ZinoChan <devzinochan@gmail.com>
2023-10-29 13:57:23 +0100

    add error page to home

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import ErrorUI from '@/shared/Error';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return <ErrorUI reset={reset} />;
+}

6918d7cf4a2bb32b25bf9b9c9c86e635eda24c31
ZinoChan <devzinochan@gmail.com>
2023-11-01 14:05:14 +0100

    feat: add user mission details page

--- src/app/profile/[missionId]/page.tsx
+++ src/app/profile/[missionId]/page.tsx
@@ -1,11 +1,36 @@
-// import Post from '@/shared/Post';
+import { xprisma } from '@/lib/prismaExtentions';
+import Post from '@/shared/Post';
+import { authOptions } from '@/utils/AuthOptions';
+import { ROUTES } from '@/utils/routes';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-const UserMissionDetails = () => {
-  return (
-    <section className='pt-10 '>
-      <div className='mx-auto max-w-screen-md'>{/* Post here */}</div>
-    </section>
-  );
-};
+export default async function UserMissionDetails({
+  params,
+}: {
+  params: { missionId: string };
+}) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    redirect(
+      `/api/auth/signin?callbackUrl=${ROUTES.USER_PROFILE}/${params.missionId}`
+    );
 
-export default UserMissionDetails;
+  const currentUserEmail = session?.user?.email;
+
+  if (currentUserEmail == null) return null;
+  const user = await xprisma.user.findByEmail(currentUserEmail);
+
+  if (!user) return null;
+
+  const mission = await xprisma.mission.getMissionById(params.missionId);
+  if (mission != null)
+    return (
+      <section className='pt-10 '>
+        <div className='mx-auto max-w-screen-md'>
+          <Post mission={mission} />
+        </div>
+      </section>
+    );
+  return <div>mission not found</div>;
+}

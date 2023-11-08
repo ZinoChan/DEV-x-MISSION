3f287b4aef14b512b2fd04c211944505bc9e7a9d
ZinoChan <devzinochan@gmail.com>
2023-11-08 13:02:57 +0100

    config: add testing configuration

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,12 @@ const createJestConfig = nextJest({
   dir: './',
 });
 const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: "node",
+  preset: 'ts-jest',
+  "moduleNameMapper": {
+  '^@/(.*)$': '<rootDir>/src/$1'
+}
 };
 export default createJestConfig(customJestConfig);
